da167ea9a0c386a7cd1114a6611a9673bb224313
Nolan Law <nolan.law@yahoo.com>
2025-07-18 17:27:46 -0500

    Initial Commit

---
+++ OneHandedSolitaire/OneHandedSolitaireApp.swift
@@ -0,0 +1,17 @@
+//
+//  OneHandedSolitaireApp.swift
+//  OneHandedSolitaire
+//
+//  Created by Nolan Law on 2025-07-18.
+//
+
+import SwiftUI
+
+@main
+struct OneHandedSolitaireApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
